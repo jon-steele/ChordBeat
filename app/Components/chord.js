123e5bebe14be8c7cf2bf1b338b4ac33a0af9055
Jon Steele <jonwhsteele@gmail.com>
2023-05-27 18:11:22 -0700

    Sets up basic chord component

---
+++ app/Components/chord.js
@@ -0,0 +1,5 @@
+export default function Chord(props) {
+    return(
+        <h>{props.chord}</h>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -2,14 +2,16 @@
 
 import { useState } from 'react';
 import Selection from './Components/selection';
+import Chord from './Components/chord';
 
-export default function Home() {
+export default function ChordBeat() {
 
   const [bpm, setBpm] = useState(100);
   const [key, setKey] = useState('C Major');
   const [timesignature, setTimesignature] = useState('44');
   const [mute, setMute] = useState(0);
   const [start, setStart] = useState(0);
+  const [chord, setChord] = useState('G Major');
 
   return (
     <html>
@@ -22,6 +24,7 @@ export default function Home() {
         
         <main class="h-screen">
           <Selection></Selection>
+          <Chord chord={chord}></Chord>
         </main>
       </body>
     </html>
