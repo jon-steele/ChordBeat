a72131e88cf56ebc75af6a7c2f7170706bf20b5b
Jon Steele <jonwhsteele@gmail.com>
2023-05-27 18:05:03 -0700

    Adds state hook variables

--- app/page.js
+++ app/page.js
@@ -1,4 +1,16 @@
+'use client';
+
+import { useState } from 'react';
+import Selection from './Components/selection';
+
 export default function Home() {
+
+  const [bpm, setBpm] = useState(100);
+  const [key, setKey] = useState('C Major');
+  const [timesignature, setTimesignature] = useState('44');
+  const [mute, setMute] = useState(0);
+  const [start, setStart] = useState(0);
+
   return (
     <html>
       <head>
@@ -7,9 +19,9 @@ export default function Home() {
 
       <body>
         <header class="w-screen text-center"> ChordBeat </header>
+        
         <main class="h-screen">
-          
-
+          <Selection></Selection>
         </main>
       </body>
     </html>
