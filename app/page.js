c4f80da0fc4dd9e2ed8a852dd24b8e4c5cbc3a03
Jon Steele <jonwhsteele@gmail.com>
2023-05-29 14:15:46 -0700

    Restructures public directory, adds timer script

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client';
-
 import { useState } from 'react';
-import Selection from './Components/selection';
-import Chord from './Components/chord';
+import Selection from '../public/components/selection';
+import Chord from '../public/components/chord';
+import Timer from '@/scripts/timer.js';
 
 export default function ChordBeat() {
 
@@ -13,6 +13,28 @@ export default function ChordBeat() {
   const [start, setStart] = useState(0);
   const [chord, setChord] = useState('G Major');
 
+  const chords = [];
+  chords.push("G Major", "C Major", "F Major");
+
+  const softSound = new Audio('audio/soft.wav');
+  const hardSound = new Audio('audio/hard.wav');
+
+  const timer = new Timer(playClick, 60000 / bpm, { immediate: true });
+
+  function playClick() {
+    softSound.play();
+    getChord(chords);
+  }
+
+  function startChords() {
+    timer.start();
+  }
+
+  function getChord(array) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    setChord(array[randomIndex]);
+  }
+
   return (
     <html>
       <head>
@@ -20,11 +42,12 @@ export default function ChordBeat() {
       </head>
 
       <body>
-        <header class="w-screen text-center"> ChordBeat </header>
+        <header className="w-screen text-center"> ChordBeat </header>
         
-        <main class="h-screen">
-          <Selection></Selection>
-          <Chord chord={chord}></Chord>
+        <main className="h-screen">
+          <Selection startChords={startChords}/>
+          <Chord chord={chord} />
+          <button onClick={playClick}>Click</button>
         </main>
       </body>
     </html>
